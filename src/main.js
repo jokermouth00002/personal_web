051fd6f1d90bb365cf515e5c54411cfec87d0cd4
TSAO <jokermouth00002@gmail.com>
2021-01-26 16:29:25 +0800

    初始版本

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'product' ? 'https://demo-server.christsao.me/personalweb' : 'http://localhost:39876/personalweb'
+})
+Vue.prototype.$api = api
+Vue.prototype.$bus = new Vue()
 new Vue({
   router,
   store,
